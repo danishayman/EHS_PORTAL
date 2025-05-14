bcbcc84bca7567e937a84b73702ef008563e3bc9
ayman <danishaiman3b@gmail.com>
2025-05-14 11:31:10 +0800

    Fixed Logout for CLIP

--- Areas/CLIP/Controllers/AccountController.cs
+++ Areas/CLIP/Controllers/AccountController.cs
@@ -471,7 +471,7 @@ namespace EHS_PORTAL.Areas.CLIP.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Welcome", "Home");
         }
 
         //
